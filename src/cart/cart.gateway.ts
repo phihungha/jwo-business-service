3acb41610ddbcb26a8d5c4f7c5286be418a5305f
Ha Phi Hung <haphihung55@gmail.com>
2024-06-08 18:23:03 +0700

    Implement cart watch event

--- src/cart/cart.gateway.ts
+++ src/cart/cart.gateway.ts
@@ -1,22 +1,37 @@
 import { UsePipes } from '@nestjs/common';
 import {
+  ConnectedSocket,
   MessageBody,
   SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 import { WsValidationPipe } from 'src/ws-validation.pipe';
 
-import { ParseJsonPipe } from '../parse-json.pipe';
 import { CartService } from './cart.service';
 import { CartUpdateDto } from './dto/cart-update.dto';
 
 @WebSocketGateway({ namespace: 'cart' })
-@UsePipes(ParseJsonPipe, new WsValidationPipe({ transform: true }))
+@UsePipes(new WsValidationPipe({ transform: true }))
 export class CartGateway {
+  @WebSocketServer()
+  private server: Server;
+
   constructor(private readonly cartService: CartService) {}
 
-  @SubscribeMessage('updateCart')
-  update(@MessageBody() body: CartUpdateDto) {
-    return this.cartService.update(body);
+  @SubscribeMessage('get')
+  async get(@ConnectedSocket() socket: Socket) {
+    socket.join('get');
+    const cart = await this.cartService.get();
+    socket.emit('cart-updated', cart);
+    return 'Begin getting latest cart.';
+  }
+
+  @SubscribeMessage('update')
+  async update(@MessageBody() body: CartUpdateDto) {
+    const cart = await this.cartService.update(body);
+    this.server.to('get').emit('cart-updated', cart);
+    return cart;
   }
 }
