43c892424f46ff330d521dca5f026d3baba2ed08
Ha Phi Hung <haphihung55@gmail.com>
2024-06-09 23:29:09 +0700

    Improve seed data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,16 +5,19 @@ const dbClient = new PrismaClient();
 async function main() {
   await dbClient.product.createMany({
     data: [
-      { id: 1, name: 'Phone', imageUrl: '', price: 5_000_000 },
-      { id: 2, name: 'Ball', imageUrl: '', price: 30_000 },
-      { id: 3, name: 'Scissor', imageUrl: '', price: 20_000 },
+      { id: 32, name: 'Ball', imageUrl: '', price: 35_000 },
+      { id: 42, name: 'Fork', imageUrl: '', price: 15_000 },
+      { id: 67, name: 'Phone', imageUrl: '', price: 5_000_000 },
+      { id: 73, name: 'Book', imageUrl: '', price: 50_000 },
+      { id: 76, name: 'Scissor', imageUrl: '', price: 20_000 },
+      { id: 79, name: 'Toothbrush', imageUrl: '', price: 13_000 },
     ],
   });
 
   await dbClient.cartItem.createMany({
     data: [
-      { productId: 2, quantity: 1 },
-      { productId: 3, quantity: 2 },
+      { productId: 67, quantity: 1 },
+      { productId: 79, quantity: 2 },
     ],
   });
 }
